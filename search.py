aff7b1aa9e5be28d60b469838569923628785663
Vitaly Khabarov <vitkhab@gmail.com>
2017-11-12 19:12:43 +0300

    Add webpage generation time

--- search.py
+++ search.py
@@ -1,5 +1,6 @@
-from flask import Flask, request
+from flask import Flask, request, g
 import postgresql
+import time
 app = Flask(__name__)
 db = postgresql.open('pq://postgres:postgres@postgres:5432/postgres')
 
@@ -7,6 +8,11 @@ get_word_id = db.prepare('SELECT ID FROM tbl_Words WHERE Word = $1;')
 get_pages_id = db.prepare('SELECT PageID FROM tbl_Words_Pages WHERE WordID = $1;')
 get_page = db.prepare('SELECT Page FROM tbl_Pages WHERE ID = $1;')
 get_page_score = db.prepare('SELECT COUNT(*) FROM tbl_Pages_Pages WHERE PageID = $1;')
+
+@app.before_request
+def before_request():
+    g.request_start_time = time.time()
+    g.request_time = lambda: "%.5fs" % (time.time() - g.request_start_time)
 
 @app.route('/')
 def start():
@@ -30,5 +36,5 @@ def start():
         pages.sort(reverse=True)
         search_query += '<br>'.join(['{} links to <a href="{}">{}</a>'.format(s, u, u) for (s, u) in pages])
 
-
-    return search_query
+    search_query += '<br>Webpage generated in {}'.format(g.request_time())
+    return search_query 
