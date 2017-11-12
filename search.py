7df7aebf0501dca6cb5ace1736a3ec6ec7ecd87c
Vitaly Khabarov <vitkhab@gmail.com>
2017-11-12 19:12:42 +0300

    Add sort method

--- search.py
+++ search.py
@@ -6,6 +6,7 @@ db = postgresql.open('pq://postgres:postgres@postgres:5432/postgres')
 get_word_id = db.prepare('SELECT ID FROM tbl_Words WHERE Word = $1;')
 get_pages_id = db.prepare('SELECT PageID FROM tbl_Words_Pages WHERE WordID = $1;')
 get_page = db.prepare('SELECT Page FROM tbl_Pages WHERE ID = $1;')
+get_page_score = db.prepare('SELECT COUNT(*) FROM tbl_Pages_Pages WHERE ReferenceID = $1;')
 
 @app.route('/')
 def start():
@@ -24,8 +25,10 @@ def start():
         pages = []
         for id in get_pages_id(word_id):
             url = get_page(id[0])[0][0]
-            pages.append('<a href="{}">{}</a>'.format(url, url))
-        search_query += '<br>'.join(pages)
+            score = get_page_score(id[0])[0][0]
+            pages.append((score, url))
+        pages.sort(reverse=True)
+        search_query += '<br>'.join(['{} links to <a href="{}">{}</a>'.format(s, u, u) for (s, u) in pages])
 
 
     return search_query
