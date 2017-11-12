0c9d10983cd998841b56111af98ae092aed96c7f
Vitaly Khabarov <vitkhab@gmail.com>
2017-11-12 19:12:41 +0300

    Add naive db operations to crawler

--- crawler.py
+++ crawler.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 from requests import get
 from argparse import ArgumentParser
 from re import findall
+import postgresql
 
 def get_page_content(url):
     page = get(url)
@@ -37,19 +38,43 @@ if __name__ == "__main__":
     to_parse = [args.url]
     parsed = []
 
-    while to_parse:
-        url = to_parse.pop()
-        (words, urls) = parse_page(url)
-        parsed.append(url)
+    with postgresql.open('pq://postgres:postgres@postgres:5432/postgres') as db:
 
-        for new_url in urls:
-            if new_url.startswith('http'):
-                pass
-            elif new_url.startswith('//'):
-                new_url = 'http:' + new_url
-            elif new_url.startswith('/'):
-                new_url = '/'.join(url.split('/')[0:3]) + new_url
-            else:
-                new_url = url.strip('/') + '/' + new_url
-            if new_url not in parsed:
-                to_parse.append(new_url)
+        get_word_id = db.prepare('SELECT ID FROM tbl_Words WHERE Word = $1;')
+        new_word = db.prepare('INSERT INTO tbl_Words (Word) VALUES ($1)')
+        new_word_page = db.prepare('INSERT INTO tbl_Words_Pages VALUES ($1, $2)')
+        get_page_id = db.prepare('SELECT ID FROM tbl_Pages WHERE Page = $1;')
+        new_page = db.prepare('INSERT INTO tbl_Pages (Page) VALUES ($1)')
+        new_page_page = db.prepare('INSERT INTO tbl_Pages_Pages VALUES ($1, $2)')
+
+        while to_parse:
+            url = to_parse.pop()
+            (words, urls) = parse_page(url)
+            print(url, words)
+            parsed.append(url)
+
+            page_id = get_page_id(url)
+            if not page_id:
+                new_page(url)
+                page_id = get_page_id(url)
+            page_id = page_id[0][0]
+
+            for word in words:
+                word_id = get_word_id(word)
+                if not word_id:
+                    new_word(word)
+                    word_id = get_word_id(word)
+                word_id = word_id[0][0]
+                new_word_page(word_id, page_id)
+
+            for new_url in urls:
+                if new_url.startswith('http'):
+                    pass
+                elif new_url.startswith('//'):
+                    new_url = 'http:' + new_url
+                elif new_url.startswith('/'):
+                    new_url = '/'.join(url.split('/')[0:3]) + new_url
+                else:
+                    new_url = url.strip('/') + '/' + new_url
+                if new_url not in parsed:
+                    to_parse.append(new_url)
