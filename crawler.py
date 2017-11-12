a0580422ccbbc6309ac12c3f2677ea9e203d1b64
Vitaly Khabarov <vitkhab@gmail.com>
2017-11-12 19:12:40 +0300

    Change crawler behaviour

--- crawler.py
+++ crawler.py
@@ -1,23 +1,56 @@
 from bs4 import BeautifulSoup
 from requests import get
+from argparse import ArgumentParser
+import re
 
-def parse_page(url):
+def get_page_content(url):
     page = get(url)
-    contents = page.content
+    return page.content
 
+def prepare_links(soup):
+    links = []
+    for link in soup.find_all('a'):
+        url = link.get('href')
+        if url:
+            links.append(url.strip())
+    return links
+
+def prepare_text(contents):
     soup = BeautifulSoup(contents, 'html.parser')
 
     for script in soup(["script", "style"]):
         script.extract()
 
-    # get text
-    text = soup.get_text()
+    return (re.findall(r"[\w']+", soup.get_text()), prepare_links(soup))
 
-    # break into lines and remove leading and trailing space on each
-    lines = (line.strip() for line in text.splitlines())
-    # break multi-headlines into a line each
-    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-    # drop blank lines
-    text = '\n'.join(chunk for chunk in chunks if chunk)
 
-    return (url, text, [link.get('href').strip() for link in soup.find_all('a')])
+def parse_page(url):
+    contents = get_page_content(url)
+
+    return prepare_text(contents)
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description='Simple web crawler')
+    parser.add_argument('url', help='URL to start')
+    args = parser.parse_args()
+
+    to_parse = [args.url]
+    parsed = []
+
+    while to_parse:
+        url = to_parse.pop()
+        (words, urls) = parse_page(url)
+        print(url, words)
+        parsed.append(url)
+
+        for new_url in urls:
+            if new_url.startswith('http'):
+                pass
+            elif new_url.startswith('//'):
+                new_url = 'http:' + new_url
+            elif new_url.startswith('/'):
+                new_url = '/'.join(url.split('/')[0:3]) + new_url
+            else:
+                new_url = url.strip('/') + '/' + new_url
+            if new_url not in parsed:
+                to_parse.append(new_url)
